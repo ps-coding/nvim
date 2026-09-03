52154b85fd511205198d1fcbc07b2d4a931d4d61
Prasham Shah <95548088+ps-coding@users.noreply.github.com>
2026-09-03 10:07:53 -0400

    fix ocaml

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ocamllsp = {
+          mason = false,
+          cmd = { "ocamllsp" },
+        },
+      },
+    },
+  },
+}
